dc22f58a2999da28222d548dea71432b9b28fab4
Subin Abid <subinabid@gmail.com>
2023-04-20 12:44:09 +0530

    Database with SQL Alchemy

--- todo/__init__.py
+++ todo/__init__.py
@@ -1,12 +1,14 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
+from todo.models import User
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
         SECRET_KEY="dev",
-        DATABASE=os.path.join(app.instance_path, "todo.sqlite"),
+        # DATABASE=os.path.join(app.instance_path, "todo.sqlite"),
+        SQLALCHEMY_DATABASE_URI="sqlite:///todo.db",
     )
 
     try:
@@ -14,8 +16,17 @@ def create_app():
     except OSError:
         pass
 
+    from todo.models import db
+
+    db.init_app(app)
+
     @app.route("/")
     def index():
         return "ToDo-Flask"
+
+    @app.route("/db")
+    def db():
+        users = User.query.all()
+        return render_template("db.html", users=users)
 
     return app
